2ffda36e59a2421a88fe7e411c9021b94a402420
SachaBsb <besombessacha@gmail.com>
2025-01-18 15:42:54 +0100

    sauvegarde avant de tout casser :)

--- kvm/update_inventory.sh
+++ kvm/update_inventory.sh
@@ -4,15 +4,21 @@
 master_ip=$(jq -r '.outputs.master_ip.value' terraform.tfstate)
 worker_ips=$(jq -r '.outputs.worker_ips.value[]' terraform.tfstate)
 
+# Initialize worker index
+worker_index=1
+
 # Generate inventory
 cat <<EOF > inventory
-[master_vm]
+[master]
 master ansible_host=$master_ip ansible_user=ubuntu ansible_ssh_private_key_file=/home/lousteau/.ssh/id_rsa_terraform
 
-
-[workers_vm]
-$(for ip in $worker_ips; do echo "worker ansible_host=$ip ansible_user=ubuntu ansible_ssh_private_key_file=/home/lousteau/.ssh/id_rsa_terraform
-"; done)
+[workers]
 EOF
+# Loop through worker IPs and increment the worker index
+for ip in $worker_ips; do
+  echo "worker$worker_index ansible_host=$ip ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa_terraform" >> inventory
+  worker_index=$((worker_index + 1))  # Increment the worker index
+done
+
 
 echo "Inventory updated successfully."
